4d43e92be89066dbc4d5720b730bdbfffd759ef5
vivvuong <63367600+vivvuong@users.noreply.github.com>
2020-04-15 14:01:51 -0500

    Styling added.
    Header styling aded for all pages

--- header.php
+++ header.php
@@ -3,48 +3,55 @@
 	require 'connect.php';
 ?>
 
-<div class="header">
-	<a class="active" href="#home"><img src="images/logo.jpg" alt="logo"></a>
-		<div class="header-right">
-			<a href="index.php">Home</a>
-			<a href="characters.php">Characters</a>
-			<a href="skills.php">Skills</a>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light ml-auto">
 
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		
+		<a class="nav-item nav-link active" href="index.php">Home</a>
+		<a class="nav-item nav-link" href="characters.php">Characters</a>
+		<a class="nav-item nav-link" href="skills.php">Skills</a>
+
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 			<?php if(isset($_SESSION['login']) && isset($_SESSION['username'])):
+
 				$query = "SELECT * FROM users WHERE username = :username";
 				$statement = $db->prepare($query);
 				$statement->bindValue(':username', $_SESSION['username']);
 				$statement->execute();
-				$result = $statement->fetch();
+				$result = $statement->fetch();?>
+
+				<a class="nav-item nav-link" href="create.php">Create</a>
+				<?php if($result['admin'] == 1):?>
+					<a class="nav-item nav-link" href="admin.php">Users</a>
+				<?php endif ?>
+
+				<nav class="navbar navbar-expand-lg navbar-light bg-light ml-auto">
+					<a class="nav-item nav-link" href="logout.php">Logout</a>
+					<h5>Welcome <a href="profile.php"><?=$_SESSION['username']?></a>!</h5>
+				</nav>
+
+				<?php 
+					$username = $_SESSION['username'];
+
+					$query = "SELECT * FROM users
+						WHERE username = :username";
+					$user = $db->prepare($query);
+					$user->bindValue(':username', $username);
+					$user->execute();
+					$result = $user->fetch();
 				?>
-					<a href="create.php">Create</a>
-					<?php if($result['admin'] == 1):?>
-						<a href="admin.php">Users</a>
-					<?php endif ?>
-					<a href="logout.php">Logout</a>
-					Welcome <a href="profile.php"><?=$_SESSION['username']?></a>!
 
-					<?php 
-						$username = $_SESSION['username'];
-
-						$query = "SELECT * FROM users
-							WHERE username = :username";
-						$user = $db->prepare($query);
-						$user->bindValue(':username', $username);
-						$user->execute();
-						$result = $user->fetch();
-					?>
-
-				
-					<img src="profile_uploads/<?=$result['profile_image_name'] . '_thumbnail' . '.' . $result['profile_image_type']?>" alt="profile_picture">
-	
-
+				<img src="profile_uploads/<?=$result['profile_image_name'] . '_thumbnail' . '.' . $result['profile_image_type']?>" alt="profile_picture">
 			<?php endif; ?>
 
 			<?php if(!isset($_SESSION['login']) || $_SESSION['login'] == false):?>
-				<a href="login.php">Login</a>
-				<a href="signup.php">Sign Up</a>
+				<a class="nav-item nav-link" href="login.php">Login</a>
+				<a class="nav-item nav-link" href="signup.php">Sign Up</a>
 			<?php endif; ?>
 		</div>
-</div>
+	</nav>
+
+
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="styles.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<title>FE: CHARACTER BUILDER</title>
 </head>
 <body>
@@ -81,7 +81,7 @@
 
 		<h1><a href="focus.php?build_id=<?=$user['build_id']?>"><?=$user['build_title']?></a></h1>
 
-		<img src="images/<?=$user['character_name']?>.png" alt="<?=$user['character_name']?>">
+		<img src="images/<?=$user['character_name']?>.png" width="400" height="auto" alt="<?=$user['character_name']?>">
 		<h1><?=$user['character_name']?> / <?=$user['class_name']?></h2>
 
 		<?php if(isset($_SESSION['login']) && isset($_SESSION['username'])):?>
@@ -157,5 +157,10 @@
 		<?php endforeach ?>
 
 	<?php endif ?>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
 </body>
 </html>
